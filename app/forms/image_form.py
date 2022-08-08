e86f63848cb73c16b3199ea0e97758f0add1d98d
Wanting Zhang <christinezhang@Wantings-MBP.lan>
2022-08-08 13:42:07 -0400

    fixed flaskform

--- app/forms/image_form.py
+++ app/forms/image_form.py
@@ -5,4 +5,4 @@ from wtforms.validators import DataRequired
 class ImageForm(FlaskForm):
     user_id = IntegerField('user_id', validators=[DataRequired()])
     business_id = IntegerField('business_id', validators=[DataRequired()])
-    image_url = StringField('image_url', validators=[DataRequired()])
+    # image_url = StringField('image_url', validators=[DataRequired()])
